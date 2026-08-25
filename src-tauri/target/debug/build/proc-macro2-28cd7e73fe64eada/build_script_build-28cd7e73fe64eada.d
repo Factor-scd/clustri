6f71eee094b697afe417171becd70d0d634dd7f8
Matt <matt@vibing.dev>
2026-08-25 20:27:30 +0000

    ci: create release before builds so upload has a target

--- src-tauri/target/debug/build/proc-macro2-28cd7e73fe64eada/build_script_build-28cd7e73fe64eada.d
+++
@@ -1,5 +0,0 @@
-/home/matt/projects/dev/clustri/src-tauri/target/debug/build/proc-macro2-28cd7e73fe64eada/build_script_build-28cd7e73fe64eada.d: /home/matt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.107/build.rs
-
-/home/matt/projects/dev/clustri/src-tauri/target/debug/build/proc-macro2-28cd7e73fe64eada/build_script_build-28cd7e73fe64eada: /home/matt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.107/build.rs
-
-/home/matt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.107/build.rs:
